93a14595c2a2ae8072c12a6f6578ec409631e5e0
Aroldo Dev <aroldodevs@gmail.com>
2022-11-01 10:02:58 +0100

    fix(files): :bug: Delete exposed API key

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,4 @@
-fetch(
-  "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyDhvQS0_Ov9k2I31EGbTUsLers8iyn7ZKc"
-)
+fetch("https://www.googleapis.com/books/v1/volumes?q=harry+potter")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
